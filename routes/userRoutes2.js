df8943ad73f6853b6b45a3a6a0238013f671f0e9
psuuc-rjnbarrientos <renzjordan0102@gmail.com>
2025-12-11 06:00:03 +0800

    annon

--- routes/userRoutes2.js
+++ routes/userRoutes2.js
@@ -8,15 +8,10 @@ const requireLogin = (req, res, next) => {
 };
 
 router.get("/new", userController.newForm);
-
 router.post("/", userController.createUser);
-
 router.get("/", requireLogin, userController.getAllUsers);
-
 router.get("/:id", userController.getUser);
-
 router.put("/:id", userController.updateUser);
-
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
